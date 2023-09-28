e33faf072f439b29a2e4d09747385db932d4825e
DaHuangGo <1160124552@qq.com>
2023-09-28 14:54:01 +0800

    测试

--- src/test/java/com/hzh/validatebean/ValidateBeanApplicationTests.java
+++ src/test/java/com/hzh/validatebean/ValidateBeanApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ValidateBeanApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
